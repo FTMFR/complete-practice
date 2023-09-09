3fc1f414c32121a5a49c9775d7c89968ac2e716a
FTMFR <fatemefarajzade018@gmail.com>
2023-09-09 18:55:53 +0330

    make input dynamic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AddUser.module.css';
 import Button from '../UI/Button';
 
 
 const AddUser = () => {
+    const [enteredUserName, setEnteredUserName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        if (enteredAge.trim().length === 0 || enteredUserName.trim().length === 0) {
+            return;
+        };
+        if (+enteredAge < 1) {
+            return;
+        };
+        setEnteredUserName('');
+        setEnteredAge('');
+
     };
+
+    const userNameChangeHandler = (event) => {
+        setEnteredUserName(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    };
+
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input type="text" id='username' />
+                <input type="text" id='username' value={enteredUserName} onChange={userNameChangeHandler} />
                 <label htmlFor='age'>Age (Years)</label>
-                <input type="number" id='age' />
+                <input type="number" id='age' value={enteredAge} onChange={ageChangeHandler} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
