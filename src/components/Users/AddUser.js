de38bc7e6d8da8f63ec79da7811c38c3b2a75e06
FTMFR <fatemefarajzade018@gmail.com>
2023-09-10 09:55:31 +0330

    create wrapper for wrap JSX

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Halper/Wrapper';
 
 
 const AddUser = (props) => {
@@ -45,7 +46,7 @@ const AddUser = (props) => {
 
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} massage={error.massage} onCalcelBtn={myErrorHandler} />}
             <Card className={styles.input}>
                 <form onSubmit={addUserHandler}>
@@ -56,8 +57,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
-
+        </Wrapper>
     );
 };
 
